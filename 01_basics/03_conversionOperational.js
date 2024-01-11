caf51e47729bea35dc11cc238c0f4cfebd8f806f
ARPIT KUMAR <99603572+ErArpitAgrahari@users.noreply.github.com>
2024-01-11 09:38:43 +0000

    conversion of dataType

---
+++ 01_basics/03_conversionOperational.js
@@ -0,0 +1,17 @@
+let score ="33"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNUmber = Number(score)
+//console.log(typeof valueInNUmber);
+
+let IsLOggedIN = 0
+let booleanIsLoggedIN = Boolean(IsLOggedIN)
+console.log(booleanIsLoggedIN);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
