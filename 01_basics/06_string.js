9ebca8eb49075e648cd5c90f50ab970ab3e3d8aa
ARPIT KUMAR <99603572+ErArpitAgrahari@users.noreply.github.com>
2024-01-11 18:41:09 +0000

    done

---
+++ 01_basics/06_string.js
@@ -0,0 +1,18 @@
+const name = "arpit"
+const repoCount = 50
+
+// console.log (name + repoCount + "value")
+
+console.log('hello my name is  ${name} and my repo count is ${repoCount}');
+
+
+const gameName = new String('hiteshhc')
+console.log(gameName[0]);
+
+console.log(gameName.__proto__);
+console.log(gameName.length);
+const newString = gameName.substring(0 , 4);
+console.log(newString);
+
+const anotherString = gameName.slice(-6 , 3);
+console.log(anotherString);
