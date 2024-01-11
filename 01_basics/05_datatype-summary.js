7899b18d2c9cf62aadd02ebff77efc8cdd3de699
ARPIT KUMAR <99603572+ErArpitAgrahari@users.noreply.github.com>
2024-01-11 16:10:54 +0000

    datatype

---
+++ 01_basics/05_datatype-summary.js
@@ -0,0 +1,45 @@
+// const score = 100
+// const scoreValue = 100
+
+// const inLOggedIn  = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotheriId = Symbol('123')
+// console.log(id == anotheriId)
+
+// const bigNumber = 32165656595656595n
+
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     age: 22
+// }
+// const myFunction = function() {
+//     console.log("hello world");
+    
+// }
+
+// console.log(typeof score);
+
+//8***************************************
+
+let myYoutubename = "arpitgrhrajhdotcom"
+let anothername =myYoutubename
+anothername = "chaiwithcahi"
+
+console.log(myYoutubename)
+console.log(anothername)
+
+let userOne ={
+    email: "bjbdkjb@google",
+    upi: "kjbdf@ybl"
+
+}
+let userTwo ={
+    email: "bjbkhddkjb@google",
+    upi: "kjbldjvdf@ybl"
+    
+}
+console.log(userOne.email);
+console.log(userTwo);
