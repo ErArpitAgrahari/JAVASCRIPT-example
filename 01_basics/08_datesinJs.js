a76dea14f0748f3d5df8e3f6c4f8581e5c2a2214
ARPIT KUMAR <99603572+ErArpitAgrahari@users.noreply.github.com>
2024-01-11 19:26:02 +0000

    done

---
+++ 01_basics/08_datesinJs.js
@@ -0,0 +1,14 @@
+// Dates
+
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+let myCreatDate = new Date(2023, 0, 12, 5, 6)
+let myCreatDate = new Date("2022-05-26")
+console.log(myCreatDate.toLocaleString());
